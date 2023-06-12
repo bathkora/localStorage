eeb8961cb530cef8e03ec3ca7cadc05cea0ad7dc
bathkora <mukesh.bathkora@gmail.com>
2023-06-12 18:55:03 +0530

    added image title, channel, views and time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Thumb from "./components/Thumb";
 
 function App() {
   return (
-    <div className="App">
-          Basic
-    </div>
+    <>
+      <div className="App">
+        <h1>Thumbnail Page</h1>
+        <div className="doi">
+          <Thumb title="Lots of Fun" views="10M" channel="CodeWithMe"></Thumb>
+          <Thumb title="New to React" views="13M" channel="CodeWithMe"></Thumb>
+          <Thumb title="Explore with React" views="22M" channel="CodeWithMe"></Thumb>
+          <Thumb title="Explore with React" views="22M" channel="CodeWithMe"></Thumb>
+          <Thumb title="Explore with React" views="22M" channel="CodeWithMe"></Thumb>
+          
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Thumb.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Thumb1 from "../images/Screenshot 2023-05-30 163131.png";
+import "./Thumb.css";
+
+const Thumb = ({title, views, channel}) => {
+  return (
+    <>
+      <div className="container">
+        <img src={Thumb1} className="img"></img>
+<div>Title:{title}</div>
+<div>Channel:{channel}</div>
+<div>{views} views <span>:</span>8 months ago</div>
+        
+      </div>
+    </>
+  );
+};
+
+export default Thumb;
